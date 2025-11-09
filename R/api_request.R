e1e5877f111a20af0b5d59f750eb2a1b37658c63
mengru-zhang-2357 <mzhang@makenacap.com>
2025-11-09 11:31:59 -0700

    Added comments to describe each line of code

--- R/api_request.R
+++ R/api_request.R
@@ -9,24 +9,25 @@
 setwd("~/Personal/Brown/BHDS2010/Assignment4_ShinyApp")
 
 # 1. Explore indicator list from WHO GHO database
-library(glue)
-library(httr)
 library(jsonlite)
 library (tidyverse)
 
 # Connect to database API
 gho_base <- "https://ghoapi.azureedge.net/api"
 
+# List of Key words to search for
 key_terms <- c("safely managed",
                "basic drinking",
-               "diarrhoeal diseases", 
+               "diarrhoea", 
                "intimate partner violence",
                "life expectancy",
-               "Child deaths",
-               "causes of deaths",
+               "by cause",
                "WASH services")
 
+# Start an empty list to store the indicator search results
 indicators_list <- list()
+
+# For each element in key terms, create its search URL. If any results return, add the Indicator Code and Indicator Name to the big list
 for (i in 1 : length(key_terms)){
   url <- paste0(gho_base, "/Indicator?",
                 "$select=IndicatorCode,IndicatorName&",
@@ -37,43 +38,58 @@ for (i in 1 : length(key_terms)){
   all_indicators <- fromJSON(url)$value 
   
   if (!is.null(all_indicators) && length(all_indicators) > 0) {
-    indicators_list[[length(indicators_list)+1]] <- all_indicators  %>% 
+    indicators_list[[length(indicators_list) + 1]] <- all_indicators  %>% 
       as_tibble() %>% 
       select(IndicatorCode, IndicatorName) 
   }
   
 }
 
+# Print the indicator list for selection
 indicators_list
 
 # 2. Explore indicator list from World Bank WDI database
 library(WDI)
 
+# Search for key words, return a list of indicator code and names
 WDIsearch("life expectancy")
-WDIsearch("School enrollment")
-WDIsearch("out of school")
+
 
 # 3. Explore indicator list from DHS database
+library(jsonlite)
+library (tidyverse)
+
+# Connect to database API
 dhs_base <- "https://api.dhsprogram.com/rest/dhs"
 
+# List of Key words to search for
 key_terms <- c("obtain drinking",
                "collecting water")
 
+# For the DHS database, first return the big list of all indicators, then filter by key words
+# Build the URL
 url <- paste0(dhs_base, "/indicators?",
               "returnFields=IndicatorId,Label,Definition,Level2&",
-              "perPage=10000&page=1")
+              "perPage=5000&page=1")
 
-json_page  <- fromJSON(url)           # DHS returns paged JSON
+# Retrieve the full list of indicators
+json_page  <- fromJSON(url)
+
+# Convert the data to a dataframe
 all_indicators <- as_tibble(json_page$Data) 
 
+# Start an empty list to store the indicator search results
 indicators_list <- list()
+
+# Loop through the key words, if we find any results in the definition or level 2 description, add them to the big indicator list
 for (i in 1:length(key_terms)) {
     hit_indicators  <- all_indicators %>% 
       filter(str_detect(Level2, key_terms[i]) | str_detect(Definition, key_terms[i]))
     if (nrow(hit_indicators) > 0) {
-      indicators_list[[length(indicators_list)+1]] <- hit_indicators
+      indicators_list[[length(indicators_list) + 1]] <- hit_indicators
     }
 }
 
+# Print the indicator list for selection
 indicators_list
 
