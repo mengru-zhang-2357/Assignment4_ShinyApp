03f7096a90d44b0038371f08b8fae3ed489241a6
mengru-zhang-2357 <mzhang@makenacap.com>
2025-11-09 14:11:03 -0700

    Start R script to process data downloaded from API

---
+++ R/data_processing.R
@@ -0,0 +1,8 @@
+#####################################################
+### This script is used to scrub data downloaded  ###   
+### and create data frames ready for building     ###
+### Shiny.                                        ###
+#####################################################
+
+# Set working directory
+setwd("~/Personal/Brown/BHDS2010/Assignment4_ShinyApp")
