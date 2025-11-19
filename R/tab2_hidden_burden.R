d38ef0b9ec4c85d572b45ee17d6ebb6f39665c22
mengru-zhang-2357 <mzhang@makenacap.com>
2025-11-19 12:59:32 -0700

    Added a few tables to assess the association between water access and physical violence and gender inequality

--- R/tab2_hidden_burden.R
+++ R/tab2_hidden_burden.R
@@ -18,26 +18,40 @@ hidden_burden_df <- read.csv("data/processed/tab2_data.csv")
 # Filter out the data corresponding to each indicator, remove unnecessary columns
 time_obtain_water <- hidden_burden_df %>% 
   filter(IndicatorName == "Time to obtain water") %>% 
-  select(-X, -Residence_Area, -IndicatorName, -Unit, -Source) 
+  select(-X, -Residence_Area, -IndicatorName, -Unit, -Source) %>% 
+  drop_na ()
 
 time_obtain_water$Indicator <- factor(time_obtain_water$Indicator, 
-                                      labels = c("Missing Info",
-                                                 "More than 30 Minutes",
+                                      labels = c("Water on Premise",
                                                  "Less than 30 Minutes",
-                                                 "Water on Premise"),
-                                      levels = c("Population with unknown or missing information on round trip time to water",
-                                                 "Population with water more than 30 minutes away round trip",
+                                                 "More than 30 Minutes",
+                                                 "Missing Info"),
+                                      levels = c("Population with water on the premises",
                                                  "Population with water 30 minutes or less away round trip",
-                                                 "Population with water on the premises"))
+                                                 "Population with water more than 30 minutes away round trip",
+                                                 "Population with unknown or missing information on round trip time to water"
+                                                 ))
 
 person_obtain_water <- hidden_burden_df %>% 
   filter(IndicatorName == "Person to obtain water",
          Indicator != "Population with no drinking water on premises") %>% 
-  select(-X, -Residence_Area, -IndicatorName, -Unit, -Source) 
+  select(-X, -Residence_Area, -IndicatorName, -Unit, -Source) %>% 
+  drop_na()
+
+person_obtain_water$Indicator <- factor(person_obtain_water$Indicator, 
+                                      labels = c("Adult Female",
+                                                 "Adult Male",
+                                                 "Child Female",
+                                                 "Child Male"),
+                                      levels = c("Adult female age 15 or older collects the drinking water",
+                                                 "Adult male age 15 or older collects the drinking water",
+                                                 "Female child under age 15 collects the drinking water",
+                                                 "Male child under age 15 collects the drinking water"))
 
 physical_violence <- hidden_burden_df %>% 
   filter(IndicatorName == "% Women experience physical violence") %>% 
-  select(-X, -Residence_Area, -IndicatorName, -Unit, -Source) 
+  select(-X, -Residence_Area, -IndicatorName, -Unit, -Source) %>% 
+  drop_na()
 
 # Create a list of data frames so we can process below via a loop
 list_A <- list(
@@ -95,11 +109,13 @@ list2env(list_Merged_A, envir = .GlobalEnv)
 # Filter out the data corresponding to each indicator, remove unnecessary columns
 safe_water <- hidden_burden_df %>% 
   filter(IndicatorName == "% Population using safely managed water") %>% 
-  select(-X, -Indicator, -IndicatorName, -Unit, -Source) 
+  select(-X, -Indicator, -IndicatorName, -Unit, -Source) %>% 
+  drop_na()
 
 basic_water <- hidden_burden_df %>% 
   filter(IndicatorName == "% Population using basic water") %>% 
-  select(-X, -Indicator, -IndicatorName, -Unit, -Source) 
+  select(-X, -Indicator, -IndicatorName, -Unit, -Source) %>% 
+  drop_na()
 
 # Create a list of data frames so we can process below via a loop
 list_B <- list(
@@ -213,19 +229,6 @@ for (i in 1: length(list_C)){
 list2env(list_Merged_C, envir = .GlobalEnv)
 
 # To research the association between water access and school enrollment, we create joins between water access and enrollment data frames
-# Join basic water with female unenrollment data
-water_female_une <- female_une %>% 
-  left_join(basic_water, by = c("Year", "Country", "Region")) %>% 
-  rename (female_unenroll = Value.x,
-          access_to_basic_water = Value.y) %>% 
-  filter(Residence_Area == "Total")
-
-# Join basic water with male unenrollment data
-water_male_une <- male_une %>% 
-  left_join(basic_water, by = c("Year", "Country", "Region")) %>% 
-  rename (male_unenroll = Value.x,
-          access_to_basic_water = Value.y) %>% 
-  filter(Residence_Area == "Total")
 
 # Combine female and male unenrollment data
 unenrollment <- bind_rows(female_une %>% mutate(Gender = "Female"),
@@ -237,5 +240,21 @@ water_une <- unenrollment %>%
           access_to_basic_water = Value.y) %>% 
   filter(Residence_Area == "Total")
 
+# Join basic water with person fetching water
+water_access_fetch <- person_obtain_water %>% 
+  left_join(basic_water, by = c("Year", "Country", "Region")) %>% 
+  rename (person_obtain_water = Indicator,
+          population_pct = Value.x,
+          access_to_basic_water = Value.y) %>% 
+  filter(Residence_Area == "Total") %>% 
+  drop_na()
+
+# Join basic water with physical violence
+water_violence <- physical_violence %>% 
+  left_join(basic_water, by = c("Year", "Country", "Region")) %>% 
+  rename (pct_violence = Value.x,
+          access_to_basic_water = Value.y) %>% 
+  filter(Residence_Area == "Total") %>% 
+  drop_na()
 
 
